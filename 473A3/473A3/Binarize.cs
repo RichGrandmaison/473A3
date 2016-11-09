07ddf0de2c8285abb2b13451cd93323c89fa083b
RichGrandmaison <rich.grandmaison@gmail.com>
2016-11-09 17:48:09 -0500

    binarize

---
+++ 473A3/473A3/Binarize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _473A3
+{
+    class Binarize
+    {
+        public Bitmap image;
+        int counter = 0;
+
+        public void SetImage(string imagePath)
+        {
+            Image rawImage = Image.FromFile(imagePath);
+            image = new Bitmap(rawImage);
+        }
+
+        //First, average out the RGB of the pixel, and set R = B = G = average(R,B,G)
+        //Second, set a threshold to decide if a pixel will be black or white
+        public void ToBlackAndWhite()
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color color = image.GetPixel(i, j);
+                    int r = color.R;
+                    int g = color.G;
+                    int b = color.B;
+                    int gray = (int)(r * 0.2989 + g * 0.5870 + b * 0.1140);
+                    int THRESHOLD = 128;
+                    int blackOrWhite = (gray > THRESHOLD) ? 255 : 0;
+                    image.SetPixel(i, j, Color.FromArgb(blackOrWhite, blackOrWhite, blackOrWhite));
+                }
+            }
+        }
+
+    }
+}
--- 473A3/473A3/Program.cs
+++ 473A3/473A3/Program.cs
@@ -10,6 +10,18 @@ namespace _473A3
     {
         static void Main(string[] args)
         {
+            Binarize binarize = new Binarize();
+
+            string pearPath = @"C:\workspace\473A3\473A3\473A3\original\pear.png";
+            string bananaPath = @"C:\workspace\473A3\473A3\473A3\original\banana.png";
+            string orangePath = @"C:\workspace\473A3\473A3\473A3\original\orange.png";
+            string titlePath = @"C:\workspace\473A3\473A3\473A3\original\title.png";
+
+            binarize.SetImage(bananaPath);
+            binarize.ToBlackAndWhite();
+            binarize.image.Save("pearBW.png");
+
+
         }
     }
 }
